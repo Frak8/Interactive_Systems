fbf4624c3b1e7111eb99e304467bd56ffa5a3aa5
Lars Imbsweiler <laim00001@stud.uni-saarland.de>
2024-05-20 20:58:49 +0200

    working servo code

--- WeMos/PillDispenser/src/main.cpp
+++ WeMos/PillDispenser/src/main.cpp
@@ -1,31 +1,30 @@
 #include <Arduino.h>
 #include <Servo.h>
 // put function declarations here:
+static void dispensePill(Servo s, int amount);
 
 Servo myServo;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  myServo.attach(D4);
+  myServo.attach(D4,500,2500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int pos;
-  while (Serial.available()>0)
-  {
-    int val = Serial.parseInt();
-    Serial.println(val);
-    myServo.write(val);
-    delay(5);
+  if (Serial.available()>0){
+    int amount = Serial.parseInt();
+    dispensePill(myServo, amount);
   }
-  
-  
-
-  
-
 }
 
 // put function definitions here:
+void dispensePill(Servo s, int amount){
+    for (int c = 0; c < amount;c++){
+      s.write(165);
+      delay(500);
+      s.write(0);
+      delay(500);
+    }
+}
